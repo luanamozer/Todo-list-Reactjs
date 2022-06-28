98b8ba6fbe8aca9c5b4402fda6a937154fba7a9a
Luana Mozer <luanamozzer1996@gmail.com>
2022-06-28 11:34:03 -0300

    component header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+
+import logoRocket from "../../assets/rocket.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logoRocket} alt="Logo Todo-List" />
+      <div className={styles.title}>
+        <span>ToDo</span>
+        <span>List</span>
+      </div>
+    </header>
+  );
+}
