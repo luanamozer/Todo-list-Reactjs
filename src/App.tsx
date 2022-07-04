7178116309494d8930fc7aad39ec8392dc86f002
Luana Mozer <luanamozzer1996@gmail.com>
2022-07-04 11:22:19 -0300

    ajuste de configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
+import { FormCreateNewTask } from "./components/FormCreateNewTask/FormCreateNewTask";
+import { ItemsList } from "./components/ItemsList/ItemsList";
 
 import styles from "./App.module.css";
 import "./global.module.css";
 
 export function App() {
   return (
-    <div className={styles.wrapper}>
+    <div>
       <Header />
+      <div className={styles.wrapper}>
+        <main>
+          <FormCreateNewTask />
+        </main>
+      </div>
     </div>
   );
 }
